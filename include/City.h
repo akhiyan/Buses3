840c1a985055bde35c9b93b0211952ae7372a789
akhiyan <akhiyan_davit@student.rau.am>
2024-03-25 18:02:18 +0400

    added algorithms

--- include/City.h
+++ include/City.h
@@ -4,7 +4,11 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <unordered_set>
 #include "Road.h"
+#include "Bus.h"
+#include "Crossroad.h"
 
 #define INFINITY 1000000
 
@@ -12,7 +16,22 @@ class City{
 private:
     std::vector<char> crossroads;
     std::vector<std::vector<int>> adj_matrix;
+    std::vector<std::vector<int>> adj_matrix_old;
+    std::vector<std::queue<char>> shortest_paths;
 
+    std::vector<std::vector<int>> floydWarshall(std::vector<std::vector<int>>& adj) {
+        for (int k = 0; k < adj.size(); k++) {
+            for (int i = 0; i < adj.size(); i++) {
+                for (int j = 0; j < adj.size(); j++) {
+                    if (adj[i][k] + adj[k][j] < adj[i][j] &&
+                        (adj[i][k] != INFINITY) && (adj[k][j] != INFINITY)) {
+                        adj[i][j] = adj[i][k] + adj[k][j];
+                    }
+                }
+            }
+        }
+        return adj_matrix;
+    }
 
 public:
 
@@ -24,15 +43,24 @@ public:
         for(int i = 0; i < N; ++i)
         {
             adj_matrix[i].resize(N);
+            for (int j = 0; j < N; ++j){
+                if(i != j){
+                    adj_matrix[i][j] = INFINITY;
+                }
+            }
         }
 
         for(int i = 0; i < _city.size(); ++i){
             Add_road(_city[i][0][0], _city[i][1][0], std::stoi(_city[i][2]));
         }
+
+        adj_matrix_old = adj_matrix;
+        floydWarshall(adj_matrix);
     }
 
     void Add_road(char root, char destination, int distance){
         adj_matrix[root  - 'A'][destination - 'A'] = distance;
+        adj_matrix[destination - 'A'][root - 'A'] = distance;
     }
 
     void Print_Matrix(){
@@ -47,9 +75,65 @@ public:
         for(i = 0; i < adj_matrix.size(); ++i){
             std::cout << (char)('A' + i) << " ";
             for(int j = 0; j < adj_matrix[i].size(); ++j){
+                if(adj_matrix[i][j] == INFINITY){
+                    std::cout << "- " ;
+                    continue;
+                }
                 std::cout << adj_matrix[i][j] << " ";
             }
             std::cout << std::endl;
+        }
+    }
+
+    void initDijkstra(Crossroad *sourceNode) {
+        for (auto node: allNodes) {
+            node.second->setDistance(INT_MAX);
+            node.second->setParent(nullptr);
+        }
+        sourceNode->setDistance(0);
+    }
+
+    void dijkstra(Crossroad *startNode) {
+        initDijkstra(startNode);
+        std::unordered_set<Crossroad*> q;
+        for (auto node: allNodes) {
+            q.insert(node.second);
+        }
+
+        while (!q.empty()) {
+            Crossroad* minNode = nullptr;
+            int minDistance = INT_MAX;
+            for (auto node: q) {
+                if (minDistance > node->getDistance()) {
+                    minDistance = node->getDistance();
+                    minNode = node;
+                }
+            }
+            if (minNode == nullptr) {
+                return;
+            }
+            q.erase(minNode);
+
+            for (auto edge: out_edges[minNode]) {
+                relax(edge);
+            }
+        }
+    }
+
+    void dijkstra(const char &startNodeValue) {
+        auto sourceNode = getNode(startNodeValue);
+        if (sourceNode != nullptr) {
+            dijkstra(sourceNode);
+        }
+    }
+
+    void relax(Road &edge) {
+        auto source = edge.getSource();
+        auto dest = edge.getDestination();
+
+        if (source->getDistance() + edge.getLabel() < dest->getDistance()) {
+            dest->setDistance(source->getDistance() + edge.getLabel());
+            dest->setParent(source);
         }
     }
 };
---
+++ include/Crossroad.h
@@ -0,0 +1,68 @@
+#ifndef BUSES3_CROSSROAD_H
+#define BUSES3_CROSSROAD_H
+
+#include <iostream>
+#include <vector>
+
+class Crossroad
+{
+private:
+    char value;
+    Crossroad* parent = nullptr;
+    Crossroad* destination = nullptr;
+    int distance = INT_MAX;
+
+public:
+    Crossroad(const char &val) : value(val) {}
+
+    //Crossroad(const char &other) : value(other.value) {}
+
+    char getValue() const {
+        return value;
+    }
+
+    int getDistance() const {
+        return distance;
+    }
+
+    std::vector<char> getPath()
+    {
+        std::vector<char> result;
+        result.push_back(value);
+        Crossroad * tmp = parent;
+        while (parent != nullptr)
+        {
+            result.push_back(parent->value);
+            parent = parent->parent;
+        }
+
+        parent = tmp;
+
+        return result;
+    }
+
+    void setDistance(int newDistance) {
+        distance = newDistance;
+    }
+
+    void setParent(Crossroad *newParent) {
+        parent = newParent;
+    }
+
+    bool operator==(const Crossroad &other) const {
+        return value == other.value;
+    }
+
+    void print() const {
+        std::cout << "Crossroad: " << getValue()
+                  << std::endl;
+    }
+
+    struct HashFunction {
+        size_t operator()(const Crossroad &node) const {
+            return std::hash<char>()(node.getValue());
+        }
+    };
+};
+
+#endif //BUSES3_CROSSROAD_H
